4331006f0da40956051f10a672ff70cdf55a1145
SashSv <83533117+SashSv@users.noreply.github.com>
2021-05-03 23:20:08 +0300

    Delete task_181_2-file.py

--- task_181_2-file.py
+++
@@ -1,7 +0,0 @@
-from task_181 import Cat
-
-cat_1 = Cat("Барон", "кот", 2)
-cat_2 = Cat("Сэм", "кот", 2)
-
-print(cat_1.get_name(), cat_1.get_sex(), cat_1.get_age())
-print(cat_2.get_name(), cat_2.get_sex(), cat_2.get_age())
