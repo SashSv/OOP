555cc66a057970008a7dfa5e967e9a9728b77cd6
SashSv <83533117+SashSv@users.noreply.github.com>
2021-05-06 12:48:51 +0300

    Add files via upload

---
+++ 1.10.1/1.10.1.py
@@ -0,0 +1,11 @@
+class Rectangle:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+rect_1 = Rectangle(5, 10, 50, 100)
+p = f" Rectangle ({rect_1.x} {rect_1.y} {rect_1.w} {rect_1.h})"
+print(p)
+print(type(p))
